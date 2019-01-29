0e23a81d94be05f08903371a154dc3ccf2c8d24b
Alexey Klopov <klw.alexey@yandex.ru>
2019-01-29 23:49:29 +0300

    Add s commands. Changes in p (print) command. Complete task.

--- arduino/calibrating/calibrating.ino
+++ arduino/calibrating/calibrating.ino
@@ -2,12 +2,12 @@
    Supported commands:
    - all.off
    - all.on
-   - s<number>.off
-   - s<number>.on
-   - s<number>.off+<digit>
-   - s<number>.off-<digit>
-   - s<number>.on+<digit>
-   - s<number>.on-<digit>
+   - s<digit>.off
+   - s<digit>.on
+   - s<digit>.off+<digit>
+   - s<digit>.off-<digit>
+   - s<digit>.on+<digit>
+   - s<digit>.on-<digit>
    - p - print two arrays
 */
 
@@ -16,17 +16,51 @@
 
 const int n = 6;
 const int srvPins[n] = {8, 9, 10, 11, 12};  // servo pins
-const int posInside[n] = {45, 90, 90, 10, 10, 10};  // "inside" positions
-const int steps[n] = {90, 90, 90, 90, 90, 90};  // movement from "inside" positions
+int posInside[n] = {45, 40, 60, 10, 20, 30};  // "inside" positions
+int steps[n] = {90, 80, 70, 60, 50, 40};  // movement from "inside" positions
 Servo srv[n];
 
-void setup() {
+void setInside(int srvNum) {
+  srv[srvNum].attach(srvPins[srvNum]);
+  srv[srvNum].write(posInside[srvNum] + steps[srvNum]);
+  delay(250);
+  srv[srvNum].detach();
+}
+
+void setOutside(int srvNum) {
+  srv[srvNum].attach(srvPins[srvNum]);
+  srv[srvNum].write(posInside[srvNum] + steps[srvNum]);
+  delay(250);
+  srv[srvNum].detach();
+}
+
+void setAllInside() {
   for (int i = 0; i < n; i++) {
-    srv[i].attach(srvPins[i]);
-    srv[i].write(posInside[i]);
-    delay(250);
-    srv[i].detach();
+    setInside(i);
   }
+}
+
+void setAllOutside() {
+  for (int i = 0; i < n; i++) {
+    setOutside(i);
+  }
+}
+
+String printArray(int arr[], int arrLength) {
+  /*
+   * >> 12, 34, 56, 78, 90
+   */
+  String str = "";
+  str += arr[0];
+  for (int i = 1; i < arrLength; i++) {
+    str += ", ";
+    str += arr[i];
+  }
+  return str;
+}
+
+void setup() {
+  setAllInside();
   Serial.begin(9600);
   Serial.println("Arduino is ready to work.");
 }
@@ -34,28 +68,62 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String request = Serial.readString();
-
-    /*
-     * a function that divides a request string into parts
-     */
-    
-    
     if (request.substring(0, 1) == "p") {
-      String pos = "";
-      String stp = "";
-      
-      for (int i = 0; i < n; i++) {
-        pos += posInside[i];
-        pos += ".";
+      Serial.println("Inner positions:");
+      Serial.println(printArray(posInside, n));
+      Serial.println("Steps:");
+      Serial.println(printArray(steps, n));
+    }
+    else if (request.substring(0, 1) == "s") {
+      String s = request.substring(1, 2);
+      int i = s.toInt() - 1;
+      if (request.substring(3, 5) == "on") {
+        if (request.substring(5, 6) == "+") {
+          String strNum = request.substring(6, 7);
+          int num = strNum.toInt();
+          steps[i] += num;
+          Serial.println(s + " in steps (outside pos) array has been increased by " + strNum);
+        }
+        else if (request.substring(5, 6) == "-") {
+          String strNum = request.substring(6, 7);
+          int num = strNum.toInt();
+          steps[i] -= num;
+          Serial.println(s + " in steps (outside pos) array has been decreased by " + strNum);
+        }
+        else {
+          setOutside(i);
+          Serial.println(s + " servo is on.");
+        }
       }
-      
-      for (int i = 0; i < n; i++) {
-        stp += steps[i];
-        stp += ".";
+      else if (request.substring(3, 6) == "off") {
+        if (request.substring(6, 7) == "+") {
+          String strNum = request.substring(7, 8);
+          int num = strNum.toInt();
+          posInside[i] += num;
+          Serial.println(s + " in posInside (inside pos) array has been increased by " + strNum);
+        }
+        else if (request.substring(6, 7) == "-") {
+          String strNum = request.substring(7, 8);
+          int num = strNum.toInt();
+          posInside[i] -= num;
+          Serial.println(s + " in posInside (inside pos) array has been decreased by " + strNum);
+        }
+        else {
+          setOutside(i);
+          Serial.println(s + " servo is off.");
+        }
       }
-      
-      Serial.println(pos);
-      Serial.println(stp);
-    }    
+    }
+    else if (request.substring(0, 3) == "all") {
+      if (request.substring(4, 6) == "on") {
+        setAllOutside();
+        Serial.println("All servos are in outside positions.");
+      }
+      else if (request.substring(4, 7) == "off") {
+        setAllInside();
+        Serial.println("All servos are in inside positions.");
+      }
+      else Serial.println("Error in 'all' command!");
+    } else Serial.println("Error!");
   }
 }
