18842d39cbee04e48acda2c75232a11fb7768b64
Alexey Klopov <klw.alexey@yandex.ru>
2019-02-07 19:09:34 +0300

    Add curr_time func

---
+++ python/curr_time.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import time
+import serial
+
+
+def curr_time(ser):
+    dt = datetime.now()
+    hour = dt.hour
+    minute = dt.minute
+    hm = str(hour) + ' ' + str(minute)
+    # print(hm)
+    # ser.write(bytes(hm, 'UTF-8'))
+    return hm
+
+
+def test_time():
+    ser = serial.Serial('COM7', '9600')
+    time.sleep(5)  # если мало "поспать", не работает
+    curr_time(ser)
+    ser.close()
+
+
+# test_time()
