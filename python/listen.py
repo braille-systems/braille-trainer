bf53fd0e793d7d556619f6672443520128818e9e
Paul Lapotnikov <lapotnikov.pv@edu.spbstu.ru>
2019-02-09 04:43:45 +0300

    add real listen_symbol

--- python/listen.py
+++ python/listen.py
@@ -1,2 +1,13 @@
-def listen_symbol():
-    return str(input())
+import serial_hex
+from PyQt5 import (QtCore, QtGui, QtWidgets)
+from ui_letter import Ui_LetterWidget
+class LetterWidget(QtWidgets.QWidget, Ui_LetterWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+    def setLetter(self,letter):
+        self.letter.setText(letter.upper())
+        data = serial_hex.charToBraille(letter).replace('0','.').replace('1','O')
+        data = data[0]+data[3]+'\n'+data[1]+data[4]+'\n'+data[2]+data[5]
+        self.braille.setText(data)
+
