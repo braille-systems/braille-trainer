55546d872a1f30ba80d04d8a1a04fa642b50fa9a
unknown <alex95ruslexa@mail.ru>
2019-03-31 00:38:45 +0300

    integrate pocketsphinx int listen.py

--- python/listen_by_sphinx.py
+++ python/listen_by_sphinx.py
@@ -1,20 +1,26 @@
 import os
 from pocketsphinx import LiveSpeech, get_model_path
 
-model_path = get_model_path()
 
-speech = LiveSpeech(
-    verbose=False,
-    sampling_rate=16000,
-    buffer_size=2048,
-    no_search=False,
-    full_utt=False,
-    hmm=os.path.join(model_path, 'zero_ru.cd_cont_4000'),
-    lm=os.path.join(model_path, 'ru.lm'),
-    dic=os.path.join(model_path, 'my_dict_out.dic')
-)
+def listen_sphinx():
+    model_path = get_model_path()
 
-print("Say something!")
+    speech = LiveSpeech(
+        verbose=False,
+        sampling_rate=8000,
+        buffer_size=2048,
+        no_search=False,
+        full_utt=False,
+        hmm=os.path.join(model_path, 'zero_ru.cd_cont_4000'),
+        lm=os.path.join(model_path, 'ru.lm'),
+        dic=os.path.join(model_path, 'my_dict_out.dic')
+    )
 
-for phrase in speech:
-    print(phrase)
+    print("Say something!")
+    for phrase in speech:
+        return phrase
+
+
+if __name__ == '__main__':
+    while True:
+        print(listen_sphinx())
