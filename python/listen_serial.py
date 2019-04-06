5af2a024f700d868b53666a4dd241f745a239419
Valerii <valera.zuev.zva@gmail.com>
2019-04-06 19:41:21 +0300

    a step to async printLine (not perfect, though working)

--- python/listen_serial.py
+++ python/listen_serial.py
@@ -4,23 +4,44 @@ import serial_get_name as sg
 import help as hp
 
 
-def listen_serial(ser, menuID=-1):
+def listen_serial(ser, menuID=-1, requiresReturn =False):
     # Returns l, r, d, u or char in format 110000 (means 'б')
     print('listen joystick or keyboard:')
-    
-    ser.flushInput()
+    #ser.flushInput()
     ser.write(bytes('?', 'UTF-8'))
     line = ser.readline()
     s = str(line)
+    while not (s[2] in 'ldru01c+'):
+        print('not in ldru01c+')
+        line = ser.readline()
+        s = str(line)
     if s[2] in 'ldru':
         return s[2]
     if s[2] in '01':
         return s[2:8]
     if (s[2] in 'c') & (menuID != -1):
         hp.instantHelp(menuID)
-    return True
-    
-    #return input()
+        #TODO: return something
+        return s[2]
+    if (s[2] == '+'):
+        if requiresReturn:
+            t = time.time()
+            critTime = 2 #delay in seconds
+            while(time.time() - t < critTime):
+                #print(time.time()-t)
+                if ser.in_waiting:
+                    print('inWaiting'+str(ser.in_waiting))
+                    line = ser.readline()
+                    s = str(line)
+                    return s[2]
+        else:
+            #works in case when we sent a char to printLine
+            # and then waiting for user's response,
+            # but the first returned character from Serial is '+'
+            # so we ignore it and listen next
+            return listen_serial(ser)
+    if(requiresReturn):
+        return '0'
 
 
 if __name__ == "__main__":
